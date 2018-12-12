9d552f21c23c98ed3adf728e13afd70e85411e10
Mayur Ugale <mayurugale@Mayurs-MacBook-Air.local>
2018-12-12 12:27:59 +0530

    project completed till now

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA_aPSfK69ZO51oOxkUcWYzR3yyDEFm7kk",
+    authDomain: "oshop-532e5.firebaseapp.com",
+    databaseURL: "https://oshop-532e5.firebaseio.com",
+    projectId: "oshop-532e5",
+    storageBucket: "oshop-532e5.appspot.com",
+    messagingSenderId: "801855588823"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA_aPSfK69ZO51oOxkUcWYzR3yyDEFm7kk",
+    authDomain: "oshop-532e5.firebaseapp.com",
+    databaseURL: "https://oshop-532e5.firebaseio.com",
+    projectId: "oshop-532e5",
+    storageBucket: "oshop-532e5.appspot.com",
+    messagingSenderId: "801855588823"
+  }
 };
 
 /*
